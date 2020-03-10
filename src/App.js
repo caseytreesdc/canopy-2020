bf5065a83d435a11ab8a7b28117e854290c1ce62
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-03-10 10:23:57 -0400

    new button to sponsorship form  - need sponsorship boxes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from "./Components/Header"
 import Masthead from "./Components/Masthead"
+import BashInfo from "./assets/the-bash.png"
+import DinnerInfo from "./assets/awards-bash.png"
 import './App.css';
 
 function App() {
@@ -8,14 +10,17 @@ function App() {
     <div className="App">
       <Header></Header>
       <Masthead></Masthead>
-      <div className="App__text-area">
-        <h1>Join Us In Celebration</h1>
-        <h2>Friday, April 24, 2020 | Dock5 @ Union Market</h2>
-        <p>
+      <div className="App__text">
+        <h1 className="App__header1">Join Us In Celebration</h1>
+        <h2 className="App__header2">Friday, April 24, 2020</h2>
+        <h2 className="App__header2">Dock5 @ Union Market</h2>
+        <p className="App__p">
           Arbor Day Bash: Celebrating the 2020 Canopy Awards is Casey Trees’ flagship event highlighting the accomplishments
           of those working to enhance DC’s tree canopy and make the city we love more livable for us all. There’s no better way to celebrate Arbor Day.
         </p>
       </div>
+      <img className="App__img" src={BashInfo}></img>
+      <img className="App__img" src={DinnerInfo}></img>
     </div>
   );
 }
--- src/Components/Masthead.js
+++ src/Components/Masthead.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Masthead.css';
 import Title from "../assets/Title.png"
-
+import formURL from "../assets/sponsorship-form.pdf"
 const Masthead = () => {
     let eventbriteURL = "https://www.eventbrite.com/e/arbor-day-bash-celebrating-the-2020-canopy-awards-tickets-90219395641?_eboga=561584328.1583778672&_ga=2.212130205.805995504.1583778672-561584328.1583778672"
     return (
         <div className="Masthead">
             <img className="Masthead__title" src={Title}></img>
-            <a className="Masthead__button" href={eventbriteURL}>Tickets</a>
+            <div className="Masthead__button-group">
+                <a className="Masthead__button" href={eventbriteURL}>Tickets</a>
+                <a className="Masthead__button" href={formURL} target="_blank">Sponsor</a>
+            </div>
         </div>
     )
 }
